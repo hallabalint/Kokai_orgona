9fb12eac6ece511d72d21a934bf4ce3a852fa58f
hallabalint <59656917+hallabalint@users.noreply.github.com>
2023-08-08 22:23:41 +0200

    optimized program

--- orgona_pedal/orgona_pedal.ino
+++ orgona_pedal/orgona_pedal.ino
@@ -1,47 +1,25 @@
-bool registers[4][8];
+#define CLK 34
 int cnt = 0;
 bool reset = false;
-bool sounds[36];
+bool sounds[36]; 
+byte registers; //global byte for the register sw
 
 void setup() {
   Serial1.begin(35000);
-  //Serial.begin(38400);
-  pinMode(2, OUTPUT);   //data bus 0
-  pinMode(3, OUTPUT);   //data bus 1
-  pinMode(4, OUTPUT);   //data bus 2
-  pinMode(5, OUTPUT);   //data bus 3
-  pinMode(6, OUTPUT);   //data bus 4
-  pinMode(7, OUTPUT);   //data bus 5
-  pinMode(8, OUTPUT);   //data bus 6
-  pinMode(9, OUTPUT);   //data bus 7
-  pinMode(10, OUTPUT);  //address 0
-  pinMode(11, OUTPUT);  //address 1
-  pinMode(12, OUTPUT);  //address 2
-  pinMode(13, OUTPUT);  // clk
-}
-
-// arduino -> data bus port map
-int pinselect(int inp) {
-  switch (inp) {
-    case 0:
-      return 2;
-    case 1:
-      return 4;
-    case 2:
-      return 3;
-    case 3:
-      return 8;
-    case 4:
-      return 9;
-    case 5:
-      return 7;
-    case 6:
-      return 5;
-    case 7:
-      return 6;
-    default:
-      return 0;
-  }
+  //PORTA
+  pinMode(22, OUTPUT);  //data bus 0
+  pinMode(23, OUTPUT);  //data bus 1
+  pinMode(24, OUTPUT);  //data bus 2
+  pinMode(25, OUTPUT);  //data bus 3
+  pinMode(26, OUTPUT);  //data bus 4
+  pinMode(27, OUTPUT);  //data bus 5
+  pinMode(28, OUTPUT);  //data bus 6
+  pinMode(29, OUTPUT);  //data bus 7
+  //PORTC
+  pinMode(37, OUTPUT);   //address 0
+  pinMode(36, OUTPUT);   //address 1
+  pinMode(35, OUTPUT);   //address 2
+  pinMode(CLK, OUTPUT);  // clk
 }
 
 /*
@@ -55,50 +33,26 @@ pin 13 -> G1 pin -> DEMUX
 */
 
 void flash() {
-  int db = 0;
-  if (sounds[24])
-    Serial.println("I");
+  //varible for output Flip-flop states
+  byte outPORTS[4] = { 0xff, 0xff, 0xff, 0xff };
+  //sounds-> FF
   for (int i = 0; i < 27; i++) {
-    registers[i / 8][i % 8] = sounds[i];
+    if (sounds[i]) {
+      outPORTS[i / 8] |= (1 << (i % 8));
+    }
   }
-  //Serial.println(registers[3][3]);
-  digitalWrite(13, LOW);
-  for (int k = 0; k < 4; k++) {
-    digitalWrite(13, LOW);
-    /*
-    */
-    digitalWrite(12, bitRead(k, 2));
-    digitalWrite(11, bitRead(k, 1));
-    digitalWrite(10, bitRead(k, 0));
-    digitalWrite(pinselect(0), !registers[k][0]);
-    digitalWrite(pinselect(1), !registers[k][1]);
-    digitalWrite(pinselect(2), !registers[k][2]);
-    digitalWrite(pinselect(3), !registers[k][3]);
-    digitalWrite(pinselect(4), !registers[k][4]);
-    digitalWrite(pinselect(5), !registers[k][5]);
-    digitalWrite(pinselect(6), !registers[k][6]);
-    digitalWrite(pinselect(7), !registers[k][7]);
+  //Register -> FF
+  outPORTS[3] |= registers; 
 
-    //delay(1);
-    //delay(5);
-    digitalWrite(13, HIGH);
-    //delay(1);
-    /*if (4) {
-      Serial.print(bitRead(k,2));
-      Serial.print(bitRead(k,1));
-      Serial.print(bitRead(k,0));
-      Serial.print(!registers[k][0]);
-      Serial.print(!registers[k][1]);
-      Serial.print(!registers[k][2]),
-      Serial.print(!registers[k][3]);
-      Serial.print(!registers[k][4]);
-      Serial.print(!registers[k][5]);
-      Serial.print(!registers[k][6]);
-      Serial.println(!registers[k][7]);
-    }*/
-    //talán delay()
-    digitalWrite(13, LOW);
-    // delay(1);
+  //Write Procedure
+  digitalWrite(CLK, LOW);
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(CLK, LOW);
+
+    PORTC = i;
+    PORTA = ~outPORTS[i];
+    digitalWrite(CLK, HIGH);
+    digitalWrite(CLK, LOW);
   }
 }
 
@@ -122,20 +76,16 @@ void loop() {
         }
         break;
       default:
-
         if (cnt < 12) {
-          /*
-          registers[(12-cnt)/8][(12-cnt)%8] = bitRead(data, 1);
-          registers[(24-cnt)/8][(24-cnt)%8] = bitRead(data, 2);
-          registers[(36-cnt)/8][(36-cnt)%8] = bitRead(data, 3);
-          */
-          //Serial.println(data);
+          //calculate aktive sound
           sounds[11 - cnt] = bitRead(data, 1);
           sounds[23 - cnt] = bitRead(data, 2);
           sounds[35 - cnt] = bitRead(data, 4);
         } else if (cnt == 12) {
+          //register
           for (int i = 3; i < 6; i++)
-            registers[3][i] = bitRead(data, i + 1);
+            registers = data;
+            registers = registers >> 1;
         }
         cnt++;
         break;
